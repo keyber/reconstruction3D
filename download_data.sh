e02789b2d8adbe424f07941aaf8522b390a4d221
SabrinaChaouche <sabriina.chaouche@gmail.com>
2019-01-27 18:50:30 +0100

    Add files via upload

---
+++ download_data.sh
@@ -0,0 +1,15 @@
+#Script pour télécharger les données depuis le cloud de l'ENPC
+
+# Les nuages de points
+wget https://cloud.enpc.fr/s/j2ECcKleA1IKNzk/download --no-check-certificate
+unzip download
+rm download
+
+wget https://cloud.enpc.fr/s/RATKsfLQUSu0JWW/download --no-check-certificate
+unzip download
+rm download
+
+wget https://cloud.enpc.fr/s/S6TCx1QJzviNHq0/download --no-check-certificate
+unzip download
+rm download
+
